1e5f7cbdcae3f094a34d7e0202aa06a1010d84ec
Abhishek-Tiwari-coder <abhis0730@gmail.com>
2021-02-09 22:23:37 +0530

    Add files via upload

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Contacts.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        ContactUsEntities101 obj = new ContactUsEntities101();
+       public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddContact(Contactdb model)
+        {
+            Contactdb cbj = new Contactdb();
+            cbj.id = model.id;
+            cbj.Fname = model.Fname;
+            cbj.Lname = model.Lname;
+            cbj.Message = model.Message;
+            cbj.email = model.email;
+            cbj.phoneno = model.phoneno;
+            obj.Contactdbs.Add(cbj);
+            obj.SaveChanges();
+            ModelState.Clear();
+            return View();
+
+        }
+    }
+}
